aaa229365f0d65e115f8093af08ce89b7da9e485
AnoushSim <39119965+AnoushSim@users.noreply.github.com>
2018-05-27 16:21:07 +0400

    Create chstring.cpp

---
+++ chstring.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+void reverse_str(char* s);
+int main()
+{
+  char arr[]="anush";
+  std::cout<<"Your string is: "<<arr<<"."<<std::endl;
+  std::cout<<"Your new reversed string is: ";
+  reverse_str(arr);
+  return 0;
+}
+void reverse_str(char* s)
+{
+  int tmp;
+  char* ptolast=s;
+  while(*ptolast!='\0')
+      ++ptolast;
+  while(s<ptolast)
+  {
+    tmp=*s;
+    *s++=*ptolast;
+    *ptolast--=tmp;
+  }
+  std::cout<<s;
+}
